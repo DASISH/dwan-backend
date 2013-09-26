4b0e95338dda4570fb67878fd2be1af74dbde73d
Olha Shkaravska <olhsha@mpi.nl>
2013-09-26 09:23:06 +0000

    fixing ticket 373: add, delete USER fro the DBintegrityService. Extending userIsInUse in the dao.
    Lots of other stuff since power crashes, including GET and POST method for annotationResource
    
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/backend/annotator-backend/trunk@3616 72633d89-856b-0410-a642-59178932f03a

--- annotator-backend/src/test/java/eu/dasish/annotation/backend/rest/AnnotationsTest.java
+++ annotator-backend/src/test/java/eu/dasish/annotation/backend/rest/AnnotationsTest.java
@@ -22,15 +22,21 @@ import com.sun.jersey.api.client.ClientResponse;
 import eu.dasish.annotation.backend.Helpers;
 import eu.dasish.annotation.backend.TestBackendConstants;
 import eu.dasish.annotation.schema.Annotation;
+import eu.dasish.annotation.schema.AnnotationResponseBody;
+import eu.dasish.annotation.schema.AnnotationResponseContent;
 import eu.dasish.annotation.schema.ObjectFactory;
 import eu.dasish.annotation.schema.ResourceREF;
+import eu.dasish.annotation.schema.ResponseBody;
+import eu.dasish.annotation.schema.SourceInfo;
+import eu.dasish.annotation.schema.SourceInfoList;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.JAXBElement;
 import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.namespace.QName;
 import org.jmock.Expectations;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -121,7 +127,7 @@ public class AnnotationsTest extends ResourcesTest{
         System.out.println("requestUrl: " + requestUrl);
         ClientResponse response = resource().path(requestUrl).delete(ClientResponse.class);
         assertEquals(200, response.getStatus());
-        assertEquals("1", response.getEntity(String.class));
+        assertEquals("1 annotation(s) deleted.", response.getEntity(String.class));
         
       
     }
@@ -132,23 +138,39 @@ public class AnnotationsTest extends ResourcesTest{
     @Test
     public void testCreateAnnotation() throws SQLException, InstantiationException, IllegalAccessException, DatatypeConfigurationException{
         System.out.println("test createAnnotation");
-        // Peter's workaround on absence of "ObjectFactory.create... for annotations
-
-        final Annotation annotationToAdd = new Annotation();
-
+        // Peter's workaround on absence of "ObjectFactory.create... for annotations        
         //final JAXBElement<Annotation> jaxbElement = new JAXBElement<Annotation>(new QName("http://www.dasish.eu/ns/addit", "annotation"), Annotation.class, null, annotationToAdd);
-        final JAXBElement<Annotation> jaxbElement = (new ObjectFactory()).createAnnotation(annotationToAdd);
-       
+        
         final String ownerString = "5";
         final Number ownerID =  5;
         final Number newAnnotationID = 6;
-        final Annotation addedAnnotation = annotationToAdd;
-        ResourceREF owner = new ResourceREF();
-        owner.setRef(ownerString);
-        addedAnnotation.setOwner(owner);
-        addedAnnotation.setURI((UUID.randomUUID()).toString());
         
-        addedAnnotation.setTimeStamp(Helpers.setXMLGregorianCalendar(Timestamp.valueOf("2013-08-12 11:25:00.383000")));
+        ResponseBody responseBody = new ResponseBody();
+        final JAXBElement<ResponseBody> jaxbElement = (new ObjectFactory()).createResponseBody(responseBody);
+        responseBody.setPermissionResponse(null);
+        AnnotationResponseBody arb =new AnnotationResponseBody();
+        responseBody.setAnnotationResponse(arb);
+        arb.setActions(null);
+        AnnotationResponseContent content = new AnnotationResponseContent();
+        arb.setContent(content);        
+        final Annotation addedAnnotation = new Annotation();
+        content.setAnnotation(addedAnnotation);
+        ResourceREF owner = new ResourceREF();
+        owner.setRef(ownerString);        
+        SourceInfoList sourceInfoList = new SourceInfoList();
+        addedAnnotation.setTargetSources(sourceInfoList);
+        addedAnnotation.setOwner(owner);
+        addedAnnotation.setURI(TestBackendConstants._TEST_SERVLET_URI+UUID.randomUUID().toString());        
+        addedAnnotation.setTimeStamp(Helpers.setXMLGregorianCalendar(Timestamp.valueOf("2013-08-12 11:25:00.383000")));        
+        SourceInfo sourceInfo = new SourceInfo();
+        sourceInfo.setLink("google.nl");
+        sourceInfo.setRef(UUID.randomUUID().toString());
+        sourceInfo.setVersion("vandaag");
+        sourceInfoList.getTargetSource().add(sourceInfo); 
+        
+        final List<Number> sources = new ArrayList<Number>();
+        sources.add(6);
+        
         mockery.checking(new Expectations() {
             {
                 oneOf(daoDispatcher).setServiceURI(with(any(String.class)));
@@ -158,11 +180,14 @@ public class AnnotationsTest extends ResourcesTest{
                 will(returnValue(ownerID));
                 
                 //oneOf(daoDispatcher).addUsersAnnotation(annotToAddJB, ownerID);
-                oneOf(daoDispatcher).addUsersAnnotation(with(aNonNull(Annotation.class)), with(aNonNull(Number.class)));
+                oneOf(daoDispatcher).addUsersAnnotation(with(aNonNull(Number.class)), with(aNonNull(Annotation.class)));
                 will(returnValue(newAnnotationID));
                 
                 oneOf(daoDispatcher).getAnnotation(newAnnotationID);
                 will(returnValue(addedAnnotation));
+                
+                oneOf(daoDispatcher).getSourcesWithNoCachedRepresentation(newAnnotationID);
+                will(returnValue(sources));
             }
         });
         
@@ -173,12 +198,15 @@ public class AnnotationsTest extends ResourcesTest{
         ClientResponse response = resource().path(requestUrl).type(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).post(ClientResponse.class, jaxbElement);
         assertEquals(200, response.getStatus());
         
-        Annotation entity = response.getEntity(Annotation.class);
-        assertEquals(addedAnnotation.getBody(), entity.getBody());
-        assertEquals(addedAnnotation.getHeadline(), entity.getHeadline());
-        assertEquals(addedAnnotation.getPermissions(), entity.getPermissions());
-        assertEquals(addedAnnotation.getTargetSources(), entity.getTargetSources());
-        assertEquals(addedAnnotation.getTimeStamp(), entity.getTimeStamp());
-        assertEquals(addedAnnotation.getOwner().getRef(), entity.getOwner().getRef());
+        ResponseBody entity = response.getEntity(ResponseBody.class); 
+        Annotation entityA = entity.getAnnotationResponse().getContent().getAnnotation();
+        assertEquals(addedAnnotation.getBody(), entityA.getBody());
+        assertEquals(addedAnnotation.getHeadline(), entityA.getHeadline());
+        assertEquals(addedAnnotation.getPermissions(), entityA.getPermissions());
+        assertEquals(addedAnnotation.getTargetSources().getTargetSource().get(0).getLink(), entityA.getTargetSources().getTargetSource().get(0).getLink());
+        assertEquals(addedAnnotation.getTargetSources().getTargetSource().get(0).getRef(), entityA.getTargetSources().getTargetSource().get(0).getRef());
+        assertEquals(addedAnnotation.getTargetSources().getTargetSource().get(0).getVersion(), entityA.getTargetSources().getTargetSource().get(0).getVersion());
+        assertEquals(addedAnnotation.getTimeStamp(), entityA.getTimeStamp());
+        assertEquals(addedAnnotation.getOwner().getRef(), entityA.getOwner().getRef());
     }
 }
