8b530f4b2881d187d28ee34a71a7c330b5c4a8cd
Olha Shkaravska <olhsha@mpi.nl>
2013-09-26 09:56:38 +0000

    fixing ticket 373: add, delete USER fro the DBintegrityService. Extending userIsInUse in the dao.
    Lots of other stuff since power crashes, including GET and POST method for annotationResource
    
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/backend/annotator-backend/trunk/annotator-backend@3618 72633d89-856b-0410-a642-59178932f03a

--- src/main/java/eu/dasish/annotation/backend/dao/impl/JdbcUserDao.java
+++ src/main/java/eu/dasish/annotation/backend/dao/impl/JdbcUserDao.java
@@ -18,7 +18,15 @@
 package eu.dasish.annotation.backend.dao.impl;
 
 import eu.dasish.annotation.backend.dao.UserDao;
+import eu.dasish.annotation.schema.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import javax.sql.DataSource;
+import org.springframework.jdbc.core.RowMapper;
 
 /**
  *
@@ -32,5 +40,85 @@ public class JdbcUserDao extends JdbcResourceDao implements UserDao {
         resourceTableName = principalTableName;
     }
 
+    
+    /////////// GETTERS //////////////////////
+    @Override 
+    public User getUser(Number internalID){
+        StringBuilder sql  = new StringBuilder("SELECT ");
+        sql.append(principalStar).append(" FROM ").append(principalTableName).append(" WHERE ").append(principal_id).append("= ? LIMIT 1");
+        List<User> result = getSimpleJdbcTemplate().query(sql.toString(), userRowMapper, internalID);
+        return (!result.isEmpty() ? result.get(0) : null);
+     }
+    
+    private final RowMapper<User> userRowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNumber) throws SQLException {
+            User result = new User();
+            result.setURI(externalIDtoURI(rs.getString(external_id)));
+            result.setDisplayName(rs.getString(principal_name));
+            result.setEMail(rs.getString(e_mail));
+            return result;
+        }
+    };
+    
+     
+    @Override
+    public boolean userIsInUse(Number userID) {
+        StringBuilder sqlPermissions  = new StringBuilder("SELECT ");
+        sqlPermissions.append(principal_id).append(" FROM ").append(permissionsTableName).append(" WHERE ").append(principal_id).append("= ? LIMIT 1");
+        List<Number> resultSources = getSimpleJdbcTemplate().query(sqlPermissions.toString(), principalIDRowMapper, userID);
+        if (resultSources.size() > 0) {
+            return true;
+        };
+        
+        StringBuilder sqlNotebooks  = new StringBuilder("SELECT ");
+        sqlNotebooks.append(owner_id).append(" FROM ").append(notebookTableName).append(" WHERE ").append(owner_id).append("= ? LIMIT 1");
+        List<Number> resultNotebooks = getSimpleJdbcTemplate().query(sqlNotebooks.toString(), ownerIDRowMapper, userID);
+        if (resultNotebooks.size() > 0) {
+            return true;
+        };
+        return false;
+    }
+    
+    
+    @Override 
+    public boolean userExists(User user){
+        String emailCriterion = user.getEMail();
+        StringBuilder sqlSources  = new StringBuilder("SELECT ");
+        sqlSources.append(principal_id).append(" FROM ").append(principalTableName).append(" WHERE ").append(e_mail).append("= ? LIMIT 1");
+        List<Number> resultSources = getSimpleJdbcTemplate().query(sqlSources.toString(), principalIDRowMapper, emailCriterion);
+        if (resultSources.size() > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+     ////////////////////////////////////////////////////////////
+     public Number addUser(User user, String remoteID){
+        UUID externalIdentifier = UUID.randomUUID();
+        String newExternalIdentifier = externalIdentifier.toString();
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("externalId", newExternalIdentifier);
+        params.put("principalName", user.getDisplayName());
+        params.put("email", user.getEMail());
+        params.put("remoteID", remoteID);
+        StringBuilder sql = new StringBuilder("INSERT INTO ");
+        sql.append(principalTableName).append("(").append(external_id).append(",").append(principal_name).append(",").append(e_mail).append(",").append(remote_id).append(" ) VALUES (:externalId, :principalName, :email, :remoteID)");
+        final int affectedRows = getSimpleJdbcTemplate().update(sql.toString(), params);
+        return (affectedRows>0 ? getInternalID(externalIdentifier) : null);
+     }
+     
+     
+     public int deleteUser(Number internalID){
+          if (userIsInUse(internalID)) {
+            return 0;
+        }        
+        StringBuilder sql = new StringBuilder("DELETE FROM ");
+        sql.append(principalTableName).append(" where ").append(principal_id).append(" = ?");
+        return getSimpleJdbcTemplate().update(sql.toString(), internalID);
+
+     }
     
 }
