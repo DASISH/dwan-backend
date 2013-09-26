f557d654af2361d1f49967090284f926d62e43d0
Olha Shkaravska <olhsha@mpi.nl>
2013-09-26 09:09:53 +0000

    fixing ticket 373: add, delete USER fro the DBintegrityService. Extending userIsInUse in the dao.
    Lots of other stuff since power crashes, including GET and POST method for annotationResource
    
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/backend/annotator-backend/trunk/annotator-backend@3609 72633d89-856b-0410-a642-59178932f03a

--- src/main/java/eu/dasish/annotation/backend/rest/AnnotationResource.java
+++ src/main/java/eu/dasish/annotation/backend/rest/AnnotationResource.java
@@ -20,17 +20,30 @@ package eu.dasish.annotation.backend.rest;
 import eu.dasish.annotation.backend.BackendConstants;
 import eu.dasish.annotation.backend.dao.DBIntegrityService;
 import eu.dasish.annotation.schema.Annotation;
+import eu.dasish.annotation.schema.AnnotationAction;
+import eu.dasish.annotation.schema.AnnotationActionList;
+import eu.dasish.annotation.schema.AnnotationActionName;
+import eu.dasish.annotation.schema.AnnotationInfoList;
+import eu.dasish.annotation.schema.AnnotationResponseBody;
+import eu.dasish.annotation.schema.AnnotationResponseContent;
 import eu.dasish.annotation.schema.ObjectFactory;
+import eu.dasish.annotation.schema.PermissionList;
+import eu.dasish.annotation.schema.ResponseBody;
+import eu.dasish.annotation.schema.SourceList;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.JAXBElement;
@@ -44,7 +57,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Path("/annotations")
 public class AnnotationResource {
-    
+
     @Autowired
     private DBIntegrityService dbIntegrityService;
     @Context
@@ -67,33 +80,126 @@ public class AnnotationResource {
         return new ObjectFactory().createAnnotation(annotation);
     }
 
+     //TODO: unit test
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    @Path("{annotationid: " + BackendConstants.regExpIdentifier + "/sources}")
+    public JAXBElement<SourceList> getAnnotationSources(@PathParam("annotationid") String ExternalIdentifier) throws SQLException {
+        final Number annotationID = dbIntegrityService.getAnnotationInternalIdentifier(UUID.fromString(ExternalIdentifier));
+        dbIntegrityService.setServiceURI(httpServletRequest.getServletPath());
+        final SourceList sourceList = dbIntegrityService.getAnnotationSources(annotationID);
+        return new ObjectFactory().createSourceList(sourceList);
+    }
+    
+    // TODO Unit test 
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    @Path("")
+    public JAXBElement<AnnotationInfoList> getFilteredAnnotations(@QueryParam("link") String link,
+    @QueryParam("text") String text,
+    @QueryParam("access") String permission,
+    @QueryParam("access") String namespace,
+    @QueryParam("after")  Timestamp after,
+    @QueryParam("before") Timestamp before
+    ) throws SQLException {
+        dbIntegrityService.setServiceURI(httpServletRequest.getServletPath());
+        
+        String remoteUser = httpServletRequest.getRemoteUser();
+        UUID userExternalID = (remoteUser != null) ? UUID.fromString(remoteUser) : null;
+        
+        final AnnotationInfoList annotationInfoList = dbIntegrityService.getFilteredAnnotationInfos(link, text, text, namespace, userExternalID, after, before);
+        return new ObjectFactory().createAnnotationInfoList(annotationInfoList);
+    }
+    
+    // TODO Unit test    
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    @Path("{annotationid: " + BackendConstants.regExpIdentifier + "/permissions}")
+    public JAXBElement<PermissionList> getAnnotationPermissions(@PathParam("annotationid") String ExternalIdentifier) throws SQLException {
+        final Number annotationID = dbIntegrityService.getAnnotationInternalIdentifier(UUID.fromString(ExternalIdentifier));
+        dbIntegrityService.setServiceURI(httpServletRequest.getServletPath());
+        final PermissionList permissionList = dbIntegrityService.getPermissionsForAnnotation(annotationID);
+        return new ObjectFactory().createPermissionList(permissionList);
+    }
+
     ///////////////////////////////////////////////////////
-    // TODO: return envelope: deleted or not deleted
+    // TODO: how to return the status code? 
     @DELETE
     @Path("{annotationid: " + BackendConstants.regExpIdentifier + "}")
     public String deleteAnnotation(@PathParam("annotationid") String externalIdentifier) throws SQLException {
         final Number annotationID = dbIntegrityService.getAnnotationInternalIdentifier(UUID.fromString(externalIdentifier));
         int[] resultDelete = dbIntegrityService.deleteAnnotation(annotationID);
         String result = Integer.toString(resultDelete[0]);
-        return result;
+        return result + " annotation(s) deleted.";
     }
 
     ///////////////////////////////////////////////////////
-    // TODO: should be returning the envelope!!!
     @POST
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
     @Path("")
-    public JAXBElement<Annotation> createAnnotation(Annotation annotation) throws SQLException {
+    public JAXBElement<ResponseBody> createAnnotation(Annotation annotation) throws SQLException {        
+        return new ObjectFactory().createResponseBody(addORupdateAnnotation(annotation, true));
+    }
+    
+    
+    ///////////////////////////////////////////////////////
+    // TODO: unit test
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("{annotationid: " + BackendConstants.regExpIdentifier + "}")
+    public JAXBElement<ResponseBody> updateAnnotation(@PathParam("annotationid") String externalIdentifier, Annotation annotation) throws SQLException, Exception { 
+        if (!(httpServletRequest.getServletPath()+externalIdentifier).equals(annotation.getURI())){
+            throw new Exception("External annotation id and the annotation id from the request body do not match");
+        }
+        return new ObjectFactory().createResponseBody(addORupdateAnnotation(annotation, false));
+    }
+    
+    
+    
+    private ResponseBody makeResponseEnvelope(Number annotationID) throws SQLException{
+        ResponseBody result = new ResponseBody();
+        result.setPermissionResponse(null);        
+        AnnotationResponseBody subresult = new AnnotationResponseBody();
+        result.setAnnotationResponse(subresult);
+        AnnotationActionList actions = new AnnotationActionList();
+        AnnotationResponseContent content = new AnnotationResponseContent();
+        AnnotationAction action = new AnnotationAction();
+        actions.setAction(action);
+        subresult.setActions(actions);
+        subresult.setContent(content);
         
+        if (annotationID != null) {
+            Annotation annotation = dbIntegrityService.getAnnotation(annotationID);
+            content.setAnnotation(annotation);
+            List<Number> cached = dbIntegrityService.getSourcesWithNoCachedRepresentation(annotationID);
+            if (cached == null) {
+                subresult.setActions(null);
+            } else {
+                if (cached.isEmpty()) {
+                    action.setAction(null);
+                }
+                else {
+                   action.setAction(AnnotationActionName.CREATE_CACHED_REPRESENTATION); 
+                }
+            }
+
+        } else {
+            content.setAnnotation(null);
+            action.setAction(null);
+        }
+        return result;
+    }
+    
+    
+    
+    private ResponseBody addORupdateAnnotation(Annotation annotation, boolean newAnnotation) throws SQLException {
         String remoteUser = httpServletRequest.getRemoteUser();
         UUID userExternalID = (remoteUser != null) ? UUID.fromString(remoteUser) : null;
-        Number userID = dbIntegrityService.getUserInternalIdentifier(userExternalID);
-        
-        dbIntegrityService.setServiceURI(httpServletRequest.getServletPath());
-        
-        Number newAnnotationID =  dbIntegrityService.addUsersAnnotation(annotation, userID);
-        Annotation newAnnotation = dbIntegrityService.getAnnotation(newAnnotationID); 
-        return (new ObjectFactory().createAnnotation(newAnnotation));
+        Number userID = dbIntegrityService.getUserInternalIdentifier(userExternalID);    
+        dbIntegrityService.setServiceURI(httpServletRequest.getServletPath());        
+        Number newAnnotationID = newAnnotation ? dbIntegrityService.addUsersAnnotation(userID, annotation) : dbIntegrityService.updateUsersAnnotation(userID, annotation);
+        return makeResponseEnvelope(newAnnotationID);
     }
 }
